f4a680aab853175c2e550b3a18e84d45275ced32
Thomas Moret (tmoret) <tmoret@student.42lausanne.ch>
2021-11-09 23:22:17 +0100

    42 Lausanne 20211122

--- norminator.sh
+++ norminator.sh
@@ -6,7 +6,7 @@
 #    By: tmoret <marvin@42lausanne.ch>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/09 18:27:22 by tmoret            #+#    #+#              #
-#    Updated: 2021/11/09 22:27:06 by tmoret           ###   ########.fr        #
+#    Updated: 2021/11/09 23:18:13 by tmoret           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,7 +21,7 @@ mv $1 tempo2
 # Applique le formatage norminette
 
 # decoupe le fichier 
-gsed -i '13,$!d' tempo2
+gsed -i '12,$!d' tempo2
 
 # remplace 2 espace par un tab
 gsed -i 's/\s\s/\t/g' tempo2
@@ -37,8 +37,16 @@ gsed -i 's/\s!=\s/CGM48/g' tempo2
 gsed -i 's/=/ = /g' tempo2
 gsed -i 's/BNK48/ = /g' tempo2
 gsed -i 's/CGM48/ != /g' tempo2
-gsed -i 's/" ,"/,/g' tempo2
+gsed -i 's/\s','\s/',' /g' tempo2
 gsed -i 's/while(/while (/g' tempo2
+gsed -i 's/+1/+ 1/g' tempo2
+
+#supprime les espace en fin de ligne
+gsed -i 's/ *$//' tempo2
+
+# supprime la derniere ligne et inser un }
+gsed -i '1d;$d' tempo2
+echo "}" >> tempo2
 
 # reconstitue le fichier
 cat tempo tempo2 > $1
