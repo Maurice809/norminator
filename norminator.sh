7535e852c41f15ab91b2f2578b6feaa2808191c6
Thomas Moret (tmoret) <tmoret@student.42lausanne.ch>
2021-11-15 20:48:24 +0100

    42 20211120 2048

--- norminator.sh
+++ norminator.sh
@@ -6,7 +6,7 @@
 #    By: tmoret <marvin@42lausanne.ch>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/09 18:27:22 by tmoret            #+#    #+#              #
-#    Updated: 2021/11/15 19:29:08 by tmoret           ###   ########.fr        #
+#    Updated: 2021/11/15 20:48:11 by tmoret           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,15 +30,32 @@ gsed -i 's/\s\s/\t/g' tempo2
 gsed -i 's/int\s/int\t/g' tempo2
 gsed -i 's/,\sint\t/, int /g' tempo2
 gsed -i 's/int\targc/int argc/g' tempo2
+
+# remplace les pranthese
 gsed -i 's/(\s/(/g' tempo2
 gsed -i 's/\s)/)/g' tempo2
+
+#remplace les =
 gsed -i 's/\s=\s/BNK48/g' tempo2
 gsed -i 's/\s!=\s/CGM48/g' tempo2
+gsed -i 's/s\==\s/AKK48/g' tempo2
+gsed -i 's/==/STU48/g' tempo2
 gsed -i 's/=/ = /g' tempo2
 gsed -i 's/BNK48/ = /g' tempo2
 gsed -i 's/CGM48/ != /g' tempo2
+gsed -i 's/AKB48/ == /g' tempo2
+gsed -i 's/STU48/ == /g' tempo2
+
+#remplace les ,
 gsed -i 's/\s','\s/',' /g' tempo2
+
+#remplace les while( par while (
 gsed -i 's/while(/while (/g' tempo2
+
+#remplace les if( par if (
+gsed -i 's/if(/if (/g' tempo2
+
+#remplce les +1 par + 1
 gsed -i 's/+1/+ 1/g' tempo2
 
 #supprime les espace en fin de ligne
